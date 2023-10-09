0067a98e2f947ebb87e6c085908e50b540e9b27c
woldejamawolde <wolde.jama@m0ringaschool.com>
2023-10-09 09:56:25 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const cats=["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(){
+    cats.push("Ralph")
+
+}
+function destructivelyPrependCat(){
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function appendCat(){
+const cats1=cats.slice()
+cats1.push("Broom")
+    return cats1
+   
+}
+function prependCat(){
+  const cats2=cats.slice()
+  cats2.unshift("Arnold")
+return cats2
+}
+
+function removeLastCat(){
+   const cats3=cats.slice()
+    cats3.pop()
+    return cats3
+}
+function removeFirstCat(){
+    const cats4=cats.slice()
+    cats4.shift()
+    return cats4
+}
